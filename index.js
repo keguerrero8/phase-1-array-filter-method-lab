c6746a9621bf0f25a73bc6c266d2840879af13cc
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-23 23:12:26 -0500

    commit tree f255c8a81810129398dcf184a565b5e62f329d91

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    let result = drivers.filter(name => name.toUpperCase() === string.toUpperCase())
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    let result = drivers.filter(name => name.substr(0,string.length) === string)
+    return result
+}
+
+function matchName(driversObj, string) {
+    return driversObj.filter(element => element.name === string)
+}
+
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' 
+//     },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' 
+//     } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' 
+//     },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' 
+//     },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' 
+//     }
+//   ];
+
+//   expect(matchName(drivers, 'Bobby')).to.eql([
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh'
+//     },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay'
+//     }
+//   ]);
+
+
